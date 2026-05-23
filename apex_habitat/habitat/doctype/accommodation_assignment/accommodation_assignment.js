384b202077333cc00e33bc46902102dd984f3883
Abdullah Al-Mutairi <aassdd1987@gmail.com>
2026-05-23 23:20:13 +0300

    ux: configure in_list_view and columns for all child tables to show important fields cleanly in grid

--- apex_habitat/habitat/doctype/accommodation_assignment/accommodation_assignment.js
+++ apex_habitat/habitat/doctype/accommodation_assignment/accommodation_assignment.js
@@ -1,6 +1,40 @@
 // Client-side script for Accommodation Assignment
 frappe.ui.form.on("Accommodation Assignment", {
+	setup: function(frm) {
+		frm.set_query("room", function() {
+			if (!frm.doc.building) {
+				return {};
+			}
+			return {
+				filters: {
+					"building": frm.doc.building
+				}
+			};
+		});
+
+		frm.set_query("bed", function() {
+			if (!frm.doc.room) {
+				return {};
+			}
+			return {
+				filters: {
+					"room": frm.doc.room,
+					"status": ["!=", "Occupied"]
+				}
+			};
+		});
+	},
+
 	refresh(frm) {
 		// DocType client lifecycle hook
+	},
+
+	building(frm) {
+		frm.set_value("room", "");
+		frm.set_value("bed", "");
+	},
+
+	room(frm) {
+		frm.set_value("bed", "");
 	}
 });
