75230ea16f1c1ceeb5e71542be56bdb94365b71f
Abdullah Al-Mutairi <aassdd1987@gmail.com>
2026-05-23 23:08:17 +0300

    UX: hide to_bed until to_room is selected and restrict options to available beds in that room

--- apex_habitat/habitat/doctype/room_bed_transfer/room_bed_transfer.js
+++ apex_habitat/habitat/doctype/room_bed_transfer/room_bed_transfer.js
@@ -1,6 +1,26 @@
 // Client-side script for Room Bed Transfer
 frappe.ui.form.on("Room Bed Transfer", {
+	setup: function(frm) {
+		// Restrict "To Bed" selection to the chosen "To Room" and only show beds that are not Occupied
+		frm.set_query("to_bed", function() {
+			if (!frm.doc.to_room) {
+				return {}; // will return nothing or all if standard, but depends_on hides it anyway
+			}
+			return {
+				filters: {
+					"room": frm.doc.to_room,
+					"status": ["!=", "Occupied"]
+				}
+			};
+		});
+	},
+	
 	refresh(frm) {
 		// DocType client lifecycle hook
+	},
+
+	to_room(frm) {
+		// clear bed if room changes
+		frm.set_value("to_bed", "");
 	}
 });
