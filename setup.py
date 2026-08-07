36e9478987839783c0330c544eb6b0604a9d6428
iabodysa <248423400+iabodysa@users.noreply.github.com>
2026-08-07 14:29:21 +0300

    Release 2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="apex",
-    version="2.9.4",
+    version="2.2.0",
     description="Apex: Frappe Framework v15 application providing the Habitat, Salis, Logistay, and Apex Core modules.",
     packages=find_packages(),
     include_package_data=True,
